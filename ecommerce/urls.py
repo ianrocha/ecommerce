ff092d49f68a210dec9a81bef4c6f27c2b387159
Ian Rocha <ianbritosr@gmail.com>
2018-07-26 17:06:23 -0300

    Add login and register pages, add static files

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -13,11 +13,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from django.conf import settings
+from django.conf.urls.static import static
+
 from django.contrib import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.home_page, name='home'),
     path('admin/', admin.site.urls),
     path('login/', views.login_page, name='login'),
+    path('register/', views.register_page, name='register')
 ]
+
+if settings.DEBUG:
+    urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
-from .forms import LoginForm
+from django.contrib.auth import authenticate, login, get_user_model
+from .forms import LoginForm, RegisterForm
 
 
 def home_page(request):
@@ -42,9 +42,20 @@ def login_page(request):
     return render(request, "auth/login.html", context)
 
 
+User = get_user_model()
+
+
 def register_page(request):
-    form = LoginForm(request.POST or None)
+    form = RegisterForm(request.POST or None)
+    context = {
+        "form": form
+    }
     if form.is_valid():
         print(form.cleaned_data)
-    return render(request, "auth/register.html", {})
+        username = form.cleaned_data.get('username')
+        email = form.cleaned_data.get('email')
+        password = form.cleaned_data.get('password')
+        new_user = User.objects.create_user(username=username, email=email, password=password)
+        print(new_user)
+    return render(request, "auth/register.html", context)
 
