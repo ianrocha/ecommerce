277f4bddb51a8a13c44d16c4f3b9308bbf79b679
Ian Rocha <ianbritosr@gmail.com>
2018-10-22 17:21:23 -0300

    Create customer for stripe (payment process)

--- billing/models.py
+++ billing/models.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from django.db import models
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 
 from accounts.models import GuestEmail
 import stripe
@@ -32,17 +32,23 @@ class BillingProfile(models.Model):
     active = models.BooleanField(default=True)
     update = models.DateTimeField(auto_now=True)
     timestamp = models.DateTimeField(auto_now_add=True)
+    customer_id = models.CharField(max_length=120, null=True, blank=True)
 
     def __str__(self):
         return self.email
 
     objects = BillingProfileManager()
 
-# def billing_profile_created_receiver(sender, instance, created, *args, **kwargs):
-#     if created:
-#         print("Send to strip/braintree")
-#         instance.customer_id = newID
-#         instance.save()
+
+def billing_profile_created_receiver(sender, instance, *args, **kwargs):
+    if not instance.customer_id and instance.email:
+        print("Send to strip/braintree")
+        customer = stripe.Customer.create(email=instance.email)
+        instance.save()
+        instance.customer_id = customer.id
+
+
+pre_save.connect(billing_profile_created_receiver, sender=BillingProfile)
 
 
 def user_created_receiver(sender, instance, created, *args, **kwargs):
