37131deb4c0f0f3072237d6f9c12fb39b2dbb42c
Ian Rocha <ianbritosr@gmail.com>
2018-12-28 18:28:04 -0300

    Improve LoginView

--- accounts/views.py
+++ accounts/views.py
@@ -8,6 +8,7 @@ from django.views.generic.edit import FormMixin
 from django.utils.http import is_safe_url
 from django.utils.safestring import mark_safe
 
+from ecommerce.mixins import NextUrlMixin, RequestFormAttachMixin
 from .models import GuestEmail, EmailActivation
 from .forms import RegisterForm, LoginForm, GuestForm, ReactivateEmailForm
 from .signals import user_logged_in
@@ -89,77 +90,18 @@ def guest_register_view(request):
     return redirect("/register/")
 
 
-class LoginView(FormView):
+class LoginView(NextUrlMixin, RequestFormAttachMixin, FormView):
     form_class = LoginForm
     success_url = '/'
     template_name = 'accounts/login.html'
+    default_next = '/'
 
     def form_valid(self, form):
-        request = self.request
-        next_ = request.GET.get('next')
-        next_post = request.POST.get('next')
-        redirect_path = next_ or next_post or None
-        email = form.cleaned_data.get("email")
-        password = form.cleaned_data.get("password")
-        user = authenticate(request, username=email, password=password)
-        if user is not None:
-            if not user.is_active:
-                messages.error(request, "This user is inactive!")
-                return super(LoginView, self).form_invalid(form)
-            login(request, user)
-            user_logged_in.send(user.__class__, instance=user, request=request)
-            try:
-                del request.session['guest_email_id']
-            except:
-                pass
-            if is_safe_url(redirect_path, request.get_host()):
-                return redirect(redirect_path)
-            else:
-                return redirect("/")
-        return super(LoginView, self).form_invalid(form)
+        next_path = self.get_next_url()
+        return redirect(next_path)
 
 
 class RegisterView(CreateView):
     form_class = RegisterForm
     template_name = 'accounts/register.html'
     success_url = '/login/'
-
-# User = get_user_model()
-# def register_page(request):
-#     form = RegisterForm(request.POST or None)
-#     context = {
-#         "form": form
-#     }
-#     if form.is_valid():
-#         form.save()
-#
-#     return render(request, "accounts/register.html", context)
-
-# def login_page(request):
-#     form = LoginForm(request.POST or None)
-#     context = {
-#         "form": form
-#     }
-#
-#     next_ = request.GET.get('next')
-#     next_post = request.POST.get('next')
-#     redirect_path = next_ or next_post or None
-#     if form.is_valid():
-#         username = form.cleaned_data.get("username")
-#         password = form.cleaned_data.get("password")
-#         user = authenticate(request, username=username, password=password)
-#         if user is not None:
-#             login(request, user)
-#
-#             try:
-#                 del request.session['guest_email_id']
-#             except:
-#                 pass
-#
-#             if is_safe_url(redirect_path, request.get_host()):
-#                 return redirect(redirect_path)
-#             else:
-#                 return redirect("/")
-#         else:
-#             print("Error")
-#     return render(request, "accounts/login.html", context)
