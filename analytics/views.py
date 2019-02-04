75512f51d2132e5cce8d1bd0b5869c115bacbf0b
Ian Rocha <ianbritosr@gmail.com>
2019-02-04 13:51:15 -0300

    Improve sales view and template, create some querysets on orders model for sales

--- analytics/views.py
+++ analytics/views.py
@@ -2,6 +2,8 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
+from orders.models import Order
+
 
 class SalesView(LoginRequiredMixin, TemplateView):
     template_name = 'analytics/sales.html'
@@ -13,4 +15,9 @@ class SalesView(LoginRequiredMixin, TemplateView):
         return super(SalesView, self).dispatch(request, *args, **kwargs)
 
     def get_context_data(self, **kwargs):
-        return super(SalesView, self).get_context_data(**kwargs)
+        context = super(SalesView, self).get_context_data(**kwargs)
+        qs = Order.objects.all()
+        context['recent_orders'] = qs.recent().not_refunded()[:5]
+        context['shipped_orders'] = qs.recent().not_refunded().by_status(status='shipped')[:5]
+        context['paid_orders'] = qs.recent().not_refunded().by_status(status='paid')[:5]
+        return context
