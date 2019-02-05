3dbebf0476e416e1aa55622e58389b2e4973a01a
Ian Rocha <ianbritosr@gmail.com>
2019-02-05 19:38:15 -0300

    Improve Sales view chart with real data from the backend

--- analytics/views.py
+++ analytics/views.py
@@ -1,3 +1,6 @@
+import datetime
+import random
+
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import JsonResponse
 from django.shortcuts import render
@@ -11,8 +14,32 @@ class SalesAjaxView(View):
     def get(self, request, *args, **kwargs):
         data = {}
         if request.user.is_staff:
-            data['labels'] = ['Mon', 'Tues', 'Weds', 'Thurs', 'Fri', 'Sat', 'Sun']
-            data['data'] = [10, 21, 32, 25, 30, 14, 50]
+            qs = Order.objects.all().by_weeks_range(weeks_ago=5, number_of_weeks=5)
+            if request.GET.get('type') == 'week':
+                days = 7
+                start_date = timezone.now().today() - datetime.timedelta(days=days-1)
+                datetime_list = [start_date + datetime.timedelta(days=x) for x in range(0, days)]
+
+                labels = [day.strftime('%a') for day in datetime_list]
+
+                sales_items = [qs.filter(updated__day=x.day, updated__month=x.month).totals_data()['total__sum'] or 0
+                               for x in datetime_list]
+                data['labels'] = labels
+                data['data'] = sales_items
+            elif request.GET.get('type') == '4weeks':
+                data['labels'] = ['Four Weeks Ago', 'Three Weeks Ago', 'Two Weeks Ago', 'Last Week', 'This Week']
+                data['data'] = [qs.by_weeks_range(weeks_ago=x, number_of_weeks=1).totals_data()['total__sum'] or 0
+                                for x in range(5, -1, -1)]
+                # current = 5
+                # data['data'] = []
+                # for i in range(0, 5):
+                #     new_qs = qs.by_weeks_range(weeks_ago=current, number_of_weeks=1)
+                #     sales_total = new_qs.totals_data()['total__sum']
+                #     if sales_total is None:
+                #         sales_total = 0
+                #     data['data'].append(sales_total)
+                #     current -= 1
+
         return JsonResponse(data)
 
 
